7ba64a355bbcd7633c5d487dd3dd4eafcec9ef2a
mojoro <45500240+mojoro@users.noreply.github.com>
2024-04-12 13:25:24 +0200

    Create basic list modal

--- src/index.js
+++ src/index.js
@@ -17,6 +17,37 @@ function createSampleStructure(catName, listName, topName, taskName){
   return [category, list, topic, task];
 }
 
+function findCategory(catName) {
+  for (const category of categories) {
+    if (category.name === catName) {
+      return category;
+    }
+  }
+  const newCat = new Category(catName);
+  return newCat;
+}
+
+const showList = document.getElementById('showListDialog');
+const listDialog = document.getElementById('listDialog');
+const listForm = document.getElementById('listForm');
+
+showList.addEventListener("click", () => listDialog.showModal());
+listForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const category = findCategory(document.querySelector('#listCategory').value);
+  const list = new List(
+    document.querySelector('#listName').value,
+    document.querySelector('#listDescription').value);
+  
+  category.addList(list);
+
+  console.log(category);
+
+  listDialog.close();
+})
+
+
 window.Category = Category;
 window.List = List;
 window.Topic = Topic;
